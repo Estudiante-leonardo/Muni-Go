9fa56a41c0c6d45f9d3bfa02152c13b99cfce888
Estudiante-leonardo <u23209845@utp.edu.pe>
2026-05-23 18:45:29 -0500

    feat(application): implementar caso de uso para obtener tramites

---
+++ backend/src/main/java/com/tramites/backend/application/usecases/GetTramitesUseCaseImpl.java
@@ -0,0 +1,19 @@
+package com.tramites.backend.application.usecases;
+
+import com.tramites.backend.domain.model.Tramite;
+import com.tramites.backend.domain.ports.in.GetTramitesUseCase;
+import com.tramites.backend.domain.ports.out.TramiteRepositoryPort;
+import java.util.List;
+
+public class GetTramitesUseCaseImpl implements GetTramitesUseCase {
+    private final TramiteRepositoryPort tramiteRepositoryPort;
+
+    public GetTramitesUseCaseImpl(TramiteRepositoryPort tramiteRepositoryPort) {
+        this.tramiteRepositoryPort = tramiteRepositoryPort;
+    }
+
+    @Override
+    public List<Tramite> execute() {
+        return tramiteRepositoryPort.findAll();
+    }
+}
